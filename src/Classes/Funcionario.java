78dee6f610dae795db0bdb13d4da4bf92e6119b8
Daniel Araujo Leal <daniellealuau@gmail.com>
2023-01-16 12:52:01 -0300

    ok

---
+++ src/Classes/Funcionario.java
@@ -0,0 +1,23 @@
+package Classes;
+
+public class Funcionario   {
+
+
+
+
+    private String nome;
+
+
+    public Funcionario(String nome) {
+        this.nome = nome;
+
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/Classes/RegistroServico.java
@@ -0,0 +1,35 @@
+package Classes;
+
+public class RegistroServico {
+
+    private Servico servico;
+    private Funcionario funcionario;
+    private Cliente cliente;
+
+
+    public RegistroServico(Cliente cliente) {
+        this.cliente = cliente;
+
+    }
+
+    public RegistroServico() {
+
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public Servico getServico() {
+        return servico;
+    }
+
+    public Funcionario getFuncionario() {
+        return funcionario;
+
+
+    }
+
+
+
+}
